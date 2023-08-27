916e23abd1e422bd240c5fbb61e879a9e0afe4b8
Abhinav Jha <abhinavrocx@gmail.com>
2023-08-27 18:54:22 +0530

    feat: top-nav + auth fix

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
 
 function Topbar() {
   return (
     <nav className="topbar">
-      <Link href='/' className="flex items-center gap-4">
-        <Image src='/assets/logo.svg' alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">OrganiConnect</p>
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          OrganiConnect
+        </p>
       </Link>
 
       <div className="flex items-center gap-1">
@@ -15,15 +17,27 @@ function Topbar() {
           <SignedIn>
             <SignOutButton>
               <div className="flex cursor-pointer">
-                <Image src='/assets/logout.svg' alt="Logout" width={24} height={24} />
+                <Image
+                  src="/assets/logout.svg"
+                  alt="Logout"
+                  width={24}
+                  height={24}
+                />
               </div>
             </SignOutButton>
           </SignedIn>
         </div>
       </div>
 
+      <OrganizationSwitcher
+        appearance={{
+          elements: {
+            organizationSwitcherTrigger: "px-4 py-2",
+          },
+        }}
+      />
     </nav>
-  )
+  );
 }
 
 export default Topbar;
